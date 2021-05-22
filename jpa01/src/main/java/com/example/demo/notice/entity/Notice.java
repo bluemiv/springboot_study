a02bb16d81be37541c074c351bbdacb8fe7fc458
taehong.kim <public.devhong@gmail.com>
2021-05-22 22:14:41 +0900

    필요없는 코드 제거

--- jpa01/src/main/java/com/example/demo/notice/entity/Notice.java
+++ jpa01/src/main/java/com/example/demo/notice/entity/Notice.java
@@ -4,8 +4,6 @@ import lombok.*;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -23,13 +21,9 @@ public class Notice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id; // PK
 
-    @Size(min = 10, max = 100, message = "제목은 10자 이상 100자 이하 입니다.")
-    @NotBlank(message = "제목은 필수 항목입니다.")
     @Column
     private String title; // 제목
 
-    @Size(min = 10, max = 1500, message = "내용은 10자 이상 1500자 이하 입니다.")
-    @NotBlank(message = "내용은 필수 항목입니다.")
     @Column
     private String contents; // 내용
 
