110d36e491e9456af6a827fcf4b207fdc9d6b84f
taehong.kim <public.devhong@gmail.com>
2021-05-22 21:39:12 +0900

    Add duplicate notice check

--- jpa01/src/main/java/com/example/demo/notice/controller/ApiNoticeController.java
+++ jpa01/src/main/java/com/example/demo/notice/controller/ApiNoticeController.java
@@ -2,12 +2,16 @@ package com.example.demo.notice.controller;
 
 import com.example.demo.notice.entity.Notice;
 import com.example.demo.notice.exception.AlreadyDeletedException;
+import com.example.demo.notice.exception.DuplicateNoticeException;
 import com.example.demo.notice.exception.NoticeNotFoundException;
 import com.example.demo.notice.model.NoticeDeleteRequest;
 import com.example.demo.notice.model.NoticeRequest;
 import com.example.demo.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,10 +30,22 @@ public class ApiNoticeController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Notice> getNotice(@PathVariable("id") long id) {
+        // id 를 통해서 Notice 글을 가지고 옴
+        // 만약, 없다면 NoticeNotFoundException 발생
         Notice notice = noticeRepository.findById(id)
                 .orElseThrow(() -> new NoticeNotFoundException("공지사항의 글이 존재하지 않습니다. id: " + id));
 
         return ResponseEntity.status(HttpStatus.OK).body(notice);
+    }
+
+    @GetMapping("/latest/{size}")
+    public ResponseEntity<Page<Notice>> noticeLatest(@PathVariable("size") int size) {
+
+        // 최근 항목 순으로 Size 만큼 Notice 글을 가지고 옴
+        Page<Notice> noticePage = noticeRepository.findAll(
+                PageRequest.of(0, size, Sort.Direction.DESC, "registeredAt"));
+
+        return ResponseEntity.status(HttpStatus.OK).body(noticePage);
     }
 
     @PostMapping({"", "/"})
@@ -37,17 +53,23 @@ public class ApiNoticeController {
         String title = noticeRequest.getTitle();
         String contents = noticeRequest.getContents();
 
-        Notice notice = Notice.builder()
-                .title(title)
-                .contents(contents)
-                .hits(0)
-                .likes(0)
-                .registeredAt(LocalDateTime.now())
-                .build();
+        // 1분 안에 동일한 Request 가 오는 경우,
+        // 중복 글로 취급하여 등록이 안되도록 DuplicationNoticeException 을 발생시킴
+        LocalDateTime checkDate = LocalDateTime.now().minusMinutes(1L);
+        if (noticeRepository.countByTitleAndContentsAndRegisteredAtGreaterThanEqual(title, contents, checkDate) > 0) {
+            throw new DuplicateNoticeException("이미 등록된 글입니다.");
+        }
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(noticeRepository.save(notice));
+                .body(noticeRepository.save(
+                        Notice.builder()
+                                .title(title)
+                                .contents(contents)
+                                .hits(0)
+                                .likes(0)
+                                .registeredAt(LocalDateTime.now())
+                                .build()));
     }
 
     @PutMapping("/{id}")
@@ -100,9 +122,10 @@ public class ApiNoticeController {
             throw new AlreadyDeletedException("공지사항 글이 이미 삭제되었습니다. id: " + id + ", Date: " + notice.getDeletedAt());
         }
 
-        notice.setDeleted(true);
-        notice.setDeletedAt(LocalDateTime.now());
+        notice.setDeleted(true)
+                .setDeletedAt(LocalDateTime.now());
 
+        // 실제로 삭제 하지 않고, deleted 플래그 값을 true 로 변경
         noticeRepository.save(notice);
     }
 
@@ -115,8 +138,8 @@ public class ApiNoticeController {
         LocalDateTime now = LocalDateTime.now();
         noticeList.forEach(notice -> {
             log.info("{}", notice);
-            notice.setDeleted(true);
-            notice.setDeletedAt(now);
+            notice.setDeleted(true)
+                    .setDeletedAt(now);
         });
 
         noticeRepository.saveAll(noticeList);
@@ -131,8 +154,8 @@ public class ApiNoticeController {
         LocalDateTime now = LocalDateTime.now();
         noticeList.forEach(notice -> {
             log.info("{}", notice);
-            notice.setDeleted(true);
-            notice.setDeletedAt(now);
+            notice.setDeleted(true)
+                    .setDeletedAt(now);
         });
 
         noticeRepository.saveAll(noticeList);
--- jpa01/src/main/java/com/example/demo/notice/repository/NoticeRepository.java
+++ jpa01/src/main/java/com/example/demo/notice/repository/NoticeRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.notice.repository;
 import com.example.demo.notice.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,6 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     Optional<List<Notice>> findByDeletedFalse();
 
     Optional<List<Notice>> findByIdIn(List<Long> idList);
+    
+    long countByTitleAndContentsAndRegisteredAtGreaterThanEqual(String title, String contents, LocalDateTime registeredAt);
 }
